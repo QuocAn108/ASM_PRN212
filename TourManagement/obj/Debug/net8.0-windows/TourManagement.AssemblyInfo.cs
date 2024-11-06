6189710a0d66aedc06a56f74044d128e34c042d8
Ryan <anhoang800140@gmail.com>
2024-11-06 13:33:13 +0700

    resolve conflict

--- TourManagement/obj/Debug/net8.0-windows/TourManagement.AssemblyInfo.cs
+++ TourManagement/obj/Debug/net8.0-windows/TourManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03278800a61e2b26f60972026ac748d8ea594052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86087828bb3ff70c2b349256af115d3e4ead6b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
